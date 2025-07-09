d3281bf7bfdf87e9b3b9c93144bbfdb0383fa6ed
Karan Mhaswadkar <karan.mhaswadkar715@gmail.com>
2025-07-09 12:28:45 +0530

    requirements installed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "GenAI Project",
+    version="0.0.0",
+    author="Karan Mhaswadkar",
+    author_email="karan.mhaswadkar715@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+
+)
